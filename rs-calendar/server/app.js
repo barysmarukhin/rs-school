91e79971a9e265957bac8970057b50a4bdc7b569
Barys Marukhin <my68land@gmail.com>
2017-08-02 23:27:06 +0300

    rs-calendar/feature: preparation for heroku deploying

--- rs-calendar/server/app.js
+++ rs-calendar/server/app.js
@@ -36,7 +36,7 @@ app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work gr
 
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, 'reactData')));
 
 // Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
